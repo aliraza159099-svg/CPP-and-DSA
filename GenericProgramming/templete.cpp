e9935ad2d4e0289c1868d87451879a5ce899d017
Ali Raza <aliraza159099@gmail.com>
2026-09-15 22:54:41 +0500

    New commits

--- GenericProgramming/templete.cpp
+++ GenericProgramming/templete.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 using namespace std;
-template <typename myNum> float m(myNum x, myNum y){
-    return (x>y)?x:y;
+// template <typename myNum> float m(myNum x, myNum y){
+//     return (x>y)?x:y;
+// }
+// int main()
+// {
+//     cout<<"The greatest No is : "<<m(1.2,3.5);
+//     return 0;
+// };
+
+template <typename data> void display(data x){
+    cout<<"You entered : "<<x<<endl;
 }
 int main()
 {
-    cout<<"The greatest No is : "<<m(1.2,3.5);
+    int a = 5;
+    display(a);
+    string deatils = "My name is Ali Raza Balghari";
+    display(deatils);
+    display(5.4);
+    display("I am learning CPP in these days.");
     return 0;
 };
